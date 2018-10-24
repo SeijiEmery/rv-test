213de32ace1c7797980467cc076d052f7b051628
Seiji Emery <seijiemery@gmail.com>
2018-10-23 23:23:31 -0700

    updated test

--- tests/add.test.s
+++ tests/add.test.s
@@ -1,8 +1,14 @@
 
+# Note that 
 test "addition test" {{
     inputs  {{ x1 1 x2 2 x3 99 }}
     outputs {{ x1 3 x2 2 x3 3 }}
 
     add x3, x1, x2
     add x1, x1, x2
+}}
+
+test "test 2" {{
+    inputs{{ x1 10 }}
+    addi x1, x0, 10
 }}
