2df7399d2837bd1c2ba79eb7f9692126c46dfd53
Seiji Emery <seijiemery@gmail.com>
2018-10-23 17:46:36 -0700

    scrapped stuff; rewriting

--- generate_test.py
+++ generate_test.py
@@ -20,11 +20,53 @@ def process_test_file (filepath):
         lines = f.read()
 
     def parse_testcases (lines, line_num = 0):
-        # pre-search + replace all test input + test output cases
-        # since parsing is done w/ regexes, this is the only, uh...
-        # sane way to do this (can't parse recursive '{{ ... }}'
-        # statements, but by parsing + removing the inner ones first,
-        # a 2-depth nesting is possible)
+        # Try matching all statements:
+        begin_stmts = list(re.finditer(test_begin_regex, lines))
+        input_stmts = list(re.finditer(test_inputs_regex, lines))
+        output_stmts = list(re.finditer(test_outputs_regex, lines))
+
+        def find_non_overlapping_end_stmts ():
+            def overlaps (m1, m2):
+                return m1.end() <= m2.end()
+            
+            input_index, output_index = 0, 0
+            for match in re.finditer(test_end_regex, lines):
+                while input_index < len(input_stmts) and input_stmts[input_index].end() < match.start():
+                    input_index += 1
+                while output_index < len(output_stmts) and output_stmts[output_index].end() < match.start():
+                    output_index += 1
+                # print("Comparing %s %s to %s"%(input_index, input_stmts[input_index] if input_index < len(input_stmts) else None, match))
+                # print("Comparing %s %s to %s"%(output_index, output_stmts[output_index] if output_index < len(output_stmts) else None, match))
+
+                if input_index < len(input_stmts) and overlaps(input_stmts[input_index], match):
+                    # print("Discarding %s"%match)
+                    continue
+                if output_index < len(output_stmts) and overlaps(output_stmts[output_index], match):
+                    # print("Discarding %s"%match)
+                    continue
+                # print("Emitting %s"%match)
+                yield match
+        end_stmts = list(find_non_overlapping_end_stmts())
+
+
+
+        # def overlaps (m1, mlist):
+        #     for m2 in mlist:
+        #         if not (
+        #             (m1.start() >= m2.start() and m1.end() >= m2.end()) or
+        #             (m1.start() <= m2.start() and m1.end() <= m2.end())
+        #         ):
+        #             return False
+        #     return True
+        # end_stmts = [
+        #     m for m in re.finditer(test_end_regex, lines)
+        #     if not overlaps(m, input_stmts) and not overlaps(m, output_stmts)
+        # ]
+        print(begin_stmts)
+        print(input_stmts)
+        print(output_stmts)
+        print(end_stmts)
+        return
 
         # Store parsed input + output statements as in-order lists of
         # match objects
