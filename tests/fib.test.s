362113451b973287dac3b13ab32738caffe582e3
Seiji Emery <seijiemery@gmail.com>
2018-10-24 21:08:54 -0700

    added fibonacci test

---
+++ tests/fib.test.s
@@ -0,0 +1,26 @@
+
+test {{
+    inputs  {{ sp 4096 a0 10 }}
+    outputs {{ sp 4096 a0 55 }}
+    call fib
+    j end
+fib:
+    addi t0, zero, 1
+    ble a0, t0, return
+    addi sp, sp, -8
+    addi t0, a0, -1
+    sd t0, 0(sp)
+    addi a0, a0, -2
+    call fib
+
+    ld t0, 0(sp)
+    sd a0, 0(sp)
+    add a0, zero, t0
+    call fib
+
+    ld t0, 0(sp)
+    add a0, a0, t0
+return:
+    ret
+end:
+}}
