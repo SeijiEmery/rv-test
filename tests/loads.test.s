125b65f954b6fa49fe82d0629e08d2161a8c085f
Seiji Emery <seijiemery@gmail.com>
2018-11-08 00:00:01 -0800

    fixed tests + output

--- tests/loads.test.s
+++ tests/loads.test.s
@@ -4,27 +4,22 @@ test {{
     outputs {{ 
         a0 16045690984833335023 
         a1 16045690984833335023
-        a2 3735928559
+        a2 209715603
         a3 48879
         a4 239
-        a5 18446744073150512879
+        a5 209715603
         a6 18446744073709534959
         a7 18446744073709551599
     }}
-    add t0, a0, zero
-    addi t1, zero, 32
-    sll a0, a0, t0
-    or  a0, a0, t0
-
-    add gp, zero, zero
-    sd a0,  100(zero)
-    ld a1,  100(zero)
-    lwu a2, 100(zero)
-    lhu a3, 100(zero)
-    lbu a4, 100(zero)
-    lw a5,  100(zero)
-    lh a6,  100(zero)
-    lb a7,  100(zero)
+    addi gp, zero, 200
+    sd a0,  0(gp)
+    ld a1,  0(gp)
+    lwu a2, 0(gp)
+    lhu a3, 0(gp)
+    lbu a4, 0(gp)
+    lw a5,  0(gp)
+    lh a6,  0(gp)
+    lb a7,  0(gp)
 }}
 
 test {{
