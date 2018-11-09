b5ffad06a0247fbe03697bdd4c4541d255836f7e
Seiji Emery <seijiemery@gmail.com>
2018-11-08 19:11:10 -0800

    updated tests from master

--- generated/loads.0.s
+++ generated/loads.0.s
@@ -1,12 +1,12 @@
 addi gp, zero, 200
-sd a0,  0(gp)
-ld a1,  0(gp)
-lwu a2, 0(gp)
-lhu a3, 0(gp)
-lbu a4, 0(gp)
-lw a5,  0(gp)
-lh a6,  0(gp)
-lb a7,  0(gp)
+    sd a0,  0(gp)
+    ld a1,  0(gp)
+    lwu a2, 0(gp)
+    lhu a3, 0(gp)
+    lbu a4, 0(gp)
+    lw a5,  0(gp)
+    lh a6,  0(gp)
+    lb a7,  0(gp)
 nop
 nop
 nop
